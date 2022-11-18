321b3174d0078f69cd35b37e30e8fd348194ead0
Gabriel Rocha <gabriel.fernandesmed@gmail.com>
2022-11-18 00:32:33 -0200

    teste

--- JavaScript/cadastro.js
+++ JavaScript/cadastro.js
@@ -1,5 +1,13 @@
-const btn_concluir = document.querySelector(".concluir")
+const btn_enviar = document.querySelector(".concluir")
 
-btn_concluir.onclick = function () {
-    open('painel_de_controle.html')
- }
+btn_enviar.onclick = function () {
+   if(document.querySelector(".nome").value=="" ||
+   document.querySelector(".nome").value.length < 2)
+   {
+   alert( "Preencha campo NOME corretamente!" );
+   document.querySelector(".nome").focus();
+   return false;
+   }
+
+   open('painel_de_controle.html')
+}
