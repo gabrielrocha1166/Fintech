1d958b8612bded3f3020999d72bda92081a2fec9
Gabriel Rocha <gabriel.fernandesmed@gmail.com>
2022-11-29 09:42:23 -0300

    Alterações em cadastro e index

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,10 +1,10 @@
-const btn_entrar = document.querySelector("#entrar")
-const btn_novo = document.querySelector("#novo_usuario")
+const btn_ja_sou_cadastrado = document.querySelector("#ja_sou_cadastrado")
+const btn_criar_conta = document.querySelector("#criar_conta")
 
-btn_entrar.onclick = function () {
+btn_ja_sou_cadastrado.onclick = function () {
     open('login.html')
  }
 
-btn_novo.onclick = function () {
+btn_criar_conta.onclick = function () {
     open('cadastro.html')
  }
